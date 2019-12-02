a396c4e32b7d55fd4fba74c291908c5e2f5a3451
Elias Schorr <elias.schorr@fhnw.ch>
2019-12-02 10:42:50 +0100

    save

--- src/main/java/ch/fhnw/dist/BloomFilter.java
+++ src/main/java/ch/fhnw/dist/BloomFilter.java
@@ -7,29 +7,34 @@ import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class BloomFilter {
     private final boolean[] array;
-
     private final List<HashFunction> hashFunctions;
 
     public BloomFilter(int m, int k) {
-        array = new int[m];
-        hashFunctions = IntStream.range(0, 1).mapToObj(i -> Hashing.murmur3_128(i)).collect(Collectors.toUnmodifiableList());
-    }
-
-    public void add(String s) {
-        hashFunctions.stream()
-                .map(hashFunction -> hashFunction.hashString(s, StandardCharsets.UTF_8).hashCode())
-                .forEach(hashCode -> array[hashCode] = true);
-    }
-
-    public void contains(String s) {
-
+        array = new boolean[m];
+        hashFunctions = IntStream.range(0, k).mapToObj(Hashing::murmur3_128).collect(Collectors.toUnmodifiableList());
     }
 
     static BloomFilter of(int n, double p) {
         //TODO calculate optimal m,k
-        return new BloomFilter(42, 42);
+        return new BloomFilter(42, 3);
+    }
+
+    public void add(String s) {
+        stream(s).forEach(hashCode -> array[hashCode] = true);
+    }
+
+
+    public boolean contains(String s) {
+        boolean match = stream(s).allMatch(hashCode -> array[hashCode]);
+        return match;
+    }
+
+    private Stream<Integer> stream(String s) {
+        return hashFunctions.stream()
+                .map(hashFunction -> hashFunction.hashString(s, StandardCharsets.UTF_8).hashCode());
     }
 }
--- src/main/java/ch/fhnw/dist/BloomFilterMain.java
+++ src/main/java/ch/fhnw/dist/BloomFilterMain.java
@@ -1,12 +1,20 @@
 package ch.fhnw.dist;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.List;
+import java.util.HashSet;
 
 public class BloomFilterMain {
     public static void main(String[] args) throws IOException {
-        List<String> words = Files.readAllLines(Paths.get("data/words.txt"));
+        Path path = Paths.get("data/words.txt");
+        var p = 0.01;
+
+        var words = new HashSet<>(Files.readAllLines(path, StandardCharsets.UTF_8));
+        BloomFilter bloomFilter = BloomFilter.of(words.size(), p);
+        words.forEach(bloomFilter::add);
+        //TODO generate random words with apache word generatro like this range 1000 not contained in words contains
     }
 }
